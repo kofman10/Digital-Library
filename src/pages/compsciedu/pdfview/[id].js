a87379fce59b9530a687df931d1c388cd3031cc0
kofman10 <kajotonis@gmal.com>
2023-07-30 18:56:44 +0100

    create a view pdf page

--- src/pages/compsciedu/pdfview/[id].js
+++ src/pages/compsciedu/pdfview/[id].js
@@ -1,37 +1,34 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 import Navbar from "@/components/Navbar/Navbar";
 
 const File = () => {
-    const {query} = useRouter()
-    let id = query.id 
-    const [file, setFile] = useState()
-    useEffect(() => {
-   
-      const getProject = async () => {
-        if(id) { 
-          try {
-            const response =  await axios.get(`/api/compsciedu/projectinfo/${id}`);
-            setFile(response.data[0])
-          } catch (error) {
-            console.log(error)     
+  const { query } = useRouter();
+  let id = query.id;
+  const [file, setFile] = useState();
+  useEffect(() => {
+    const getProject = async () => {
+      if (id) {
+        try {
+          const response = await axios.get(`/api/compsciedu/projectinfo/${id}`);
+          setFile(response.data[0]);
+        } catch (error) {
+          console.log(error);
         }
-        }
-        }
-        getProject();
+      }
+    };
+    getProject();
   }, [id]);
 
   return (
-
     <div>
-        <Navbar />
-        <div className='pt-20 w-full h-screen'>
-
-        <object className='w-full h-full' data={file?.filename} type="application/pdf" width='500' height='500'></object>
-        </div>
+      <Navbar />
+      <div className="pt-20 w-full h-screen">
+        <iframe className="w-full h-full" src={file?.filename} width="500" height="500"></iframe>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default File
+export default File;
